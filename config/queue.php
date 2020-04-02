cdfbb4d14afd19c5bef43c82e391dd52cbaabcb4
Viet Vu <jooservices@gmail.com>
2020-04-02 20:03:46 +0700

    ^ Restore default queue config

--- config/queue.php
+++ config/queue.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('QUEUE_CONNECTION', 'database'),
+    'default' => env('QUEUE_CONNECTION', 'sync'),
 
     /*
     |--------------------------------------------------------------------------
@@ -38,7 +38,7 @@ return [
             'driver' => 'database',
             'table' => 'jobs',
             'queue' => 'default',
-            'retry_after' => 90,
+            'retry_after' => 180,
         ],
 
         'beanstalkd' => [
