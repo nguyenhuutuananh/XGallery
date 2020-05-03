11660792b7a520d64c919af1b7d8a8cd8abe841a
Viet Vu <jooservices@gmail.com>
2020-05-04 03:57:00 +0700

    ^ Update

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -96,7 +96,41 @@
         </div>
         <div class="row">
             <div class="col-12">
-                @include('macros.general.flash.messages')
+                @if ($message = Session::get('success'))
+                    <div class="alert alert-success alert-block">
+                        <button type="button" class="close" data-dismiss="alert">×</button>
+                        <strong>{!! $message !!}</strong>
+                    </div>
+                @endif
+
+                @if ($message = Session::get('error'))
+                    <div class="alert alert-danger alert-block">
+                        <button type="button" class="close" data-dismiss="alert">×</button>
+                        <strong>{!! $message !!}</strong>
+                    </div>
+                @endif
+
+                @if ($message = Session::get('warning'))
+                    <div class="alert alert-warning alert-block">
+                        <button type="button" class="close" data-dismiss="alert">×</button>
+                        <strong>{!! $message !!}</strong>
+                    </div>
+                @endif
+
+                @if ($message = Session::get('info'))
+                    <div class="alert alert-info alert-block">
+                        <button type="button" class="close" data-dismiss="alert">×</button>
+                        <strong>{!! $message !!}</strong>
+                    </div>
+                @endif
+
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <button type="button" class="close" data-dismiss="alert">×</button>
+                        Please check the form below for errors
+                    </div>
+                @endif
+
             </div>
             <div class="col-12">
                 @yield('content')
